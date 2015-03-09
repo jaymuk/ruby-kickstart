8ba60938220dc085f4dc58912a01af516153f49c
Jay M <jaymuk.business@gmail.com>
2015-03-09 17:22:38 +0000

    Add 2:1

--- session2/3-challenge/1_input_output.rb
+++ session2/3-challenge/1_input_output.rb
@@ -8,6 +8,8 @@
 # "11\n7\n18\n" to standard output.
 
 def sum_difference_product
-  # your code goes here
-  
+  input = gets().split(" ") # take the input from user and split into array
+  no1 = input[0].to_i # turn the string input into integers and assign the two array values to variable
+  no2 = input[1].to_i
+  print "#{no1 + no2}\n#{no1 - no2}\n#{no1 * no2}\n" # print string using interpolation
 end
