76e1f44e0f67301c70400c9e5c0ac0dbdc1d6902
Jay M <jaymuk.business@gmail.com>
2015-03-09 10:47:15 +0000

    Add 1-7

--- session1/3-challenge/7_string.rb
+++ session1/3-challenge/7_string.rb
@@ -4,6 +4,17 @@
 # pirates_say_arrrrrrrrr("Katy Perry is on the radio!")   # => "rya"
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
+=begin
 def pirates_say_arrrrrrrrr(string)
+	new_string = ""
+	string.size.times do |char|
+	next if string[char] == string[/(?<=r)./]
+	new_string << string[char]
+	end
+	new_string
+end
+=end
 
+def pirates_say_arrrrrrrrr(string)
+	string.scan(/(?<=[rR])./).join # use .scan to find match of - any one character that comes after 'r' or 'R'
 end
