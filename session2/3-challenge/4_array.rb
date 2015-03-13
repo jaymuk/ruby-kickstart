9d978bd90f396345c69bf64d6b703548e2799355
jaymuk <jaymuk.business@gmail.com>
2015-03-13 16:55:28 +0000

    Add 2:4

--- session2/3-challenge/4_array.rb
+++ session2/3-challenge/4_array.rb
@@ -8,5 +8,18 @@
 
 # This time you will have to define the method, it's called: get_squares
 
+def get_squares(array)
+	
+	new_array = [] # make new array to hold the values that we have specified
 
+	array.each do |num| # pass each OBJECT(object itself - not index of object) of array into block
+		new_array << num if array.include?(num * num) # check if array includes square of current object it has been passed, if it does, then put that object in new_array array we made earlier
+	end
 
+	return new_array.sort # return the new array containing objects which matched the condition
+
+end
+
+# get_squares([4,7,8,2,9,3,81]) # => [2, 3, 9]
+
+# ~> 	from -:18:in `<main>'
